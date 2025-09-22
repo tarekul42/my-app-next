772fcc462d5ac135ec0eb137c261c61526d3c478
tarekul42 <tarekulrifat205@gmail.com>
2025-09-22 18:17:15 +0600

    explored about css built in module support

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,7 +1,9 @@
+import styles from "./about.module.css";
+
 const AboutPage = () => {
   return (
     <div>
-      <h1>This is AboutPage component</h1>
+      <h1 className={styles.text_style}>This is AboutPage component</h1>
     </div>
   );
 };
--- src/app/(commonLayout)/contact/page.tsx
+++ src/app/(commonLayout)/contact/page.tsx
@@ -1,7 +1,9 @@
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
   return (
     <div>
-      <h1>This is ContactPage component</h1>
+      <h1 className={styles.text_style}>This is ContactPage component</h1>
     </div>
   );
 };
