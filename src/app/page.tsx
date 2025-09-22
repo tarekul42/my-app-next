cf898a2ee99811003e16a1714626a2d4accbed38
tarekul42 <tarekulrifat205@gmail.com>
2025-09-22 23:57:42 +0600

    font optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 const page = () => {
   return (
     <div>
-      <h1>This is page component</h1>
+      <h1 className="text-5xl text-center mt-5">This is page component</h1>
+      <p className="w-1/2 mx-auto py-5 text-2xl text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus quae laudantium placeat officiis nobis, quam eius sequi enim earum quidem maxime vero molestias perferendis dolorum ex cum ab accusantium veritatis quo odio amet expedita laborum? Quaerat, pariatur deleniti, repudiandae praesentium ullam, beatae quis debitis sapiente magni qui neque amet. Sapiente?</p>
     </div>
   );
 };
