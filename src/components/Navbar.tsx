0dd75831e3a8397f6bc0c2f8e6c8af58d9c3aeba
tarekul42 <tarekulrifat205@gmail.com>
2025-09-23 08:22:16 +0600

    explored handling active links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,43 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  
   return (
     <div className="flex justify-around py-6 bg-cyan-950">
-      <Link href="/">Home</Link>
-      <Link href="/gallery">Gallery</Link>
-      <Link href="/products">products</Link>
+      <Link
+        href="/"
+        className={`${
+          pathname === "/"
+            ? "font-semibold text-amber-100 hover:underline hover:text-amber-200"
+            : "text-white"
+        }`}
+      >
+        Home
+      </Link>
+      <Link
+        href="/gallery"
+        className={`${
+          pathname === "/gallery"
+            ? "font-semibold text-amber-100 hover:underline hover:text-amber-200"
+            : "text-white"
+        }`}
+      >
+        Gallery
+      </Link>
+      <Link
+        href="/products"
+        className={`${
+          pathname === "/products"
+            ? "font-semibold text-amber-100 hover:underline hover:text-amber-200"
+            : "text-white"
+        }`}
+      >
+        Products
+      </Link>
     </div>
   );
 };
